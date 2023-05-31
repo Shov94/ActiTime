ea54d77c2e1f8f0b41f529abc6d01be811980ac4
KARMA <KARMA@AYANOKOJI>
2023-05-31 13:18:17 +0530

    commit

---
+++ Actitime/src/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,14 @@
+package com.actitime.testscript;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(com.actitime.generic.ListenerImplement.class)
+public class CustomerModule {
+	
+@Test
+public void CreateCustomer() {
+	
+}
+
+}
